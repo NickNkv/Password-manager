537296f01c7ca0c318f5b1762a8cef0ebef3fc69
Nick <nikolay.neykov178@gmail.com>
2026-06-08 21:07:57 +0300

    abstract Cipher class added

---
+++ Password-Manager/Password-Manager/Cipher.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+
+class Cipher {
+public:
+	virtual char* encrypt(const char* text) const = 0;
+	virtual char* decrypt(const char* text) const = 0;
+	virtual void serialize(std::ostream& out) const = 0;
+	
+	virtual Cipher* clone() const = 0;
+	virtual const char* getType() const = 0;
+
+	virtual ~Cipher() = default;
+};
