b518a924f178c1638f3e0eddf07106afc53bd0aa
Nick <nikolay.neykov178@gmail.com>
2026-06-16 00:38:35 +0300

    CaeserCipher beta ready!

--- Password-Manager/Password-Manager/CaesarCipher.cpp
+++ Password-Manager/Password-Manager/CaesarCipher.cpp
@@ -10,6 +10,7 @@ CaeserCipher::CaeserCipher(int shift)
 	this->shift = shift;
 }
 
+//overrides
 char* CaeserCipher::encrypt(const char* text) const
 {
 	if (!text || strlen(text) == 0) {
@@ -29,7 +30,7 @@ char* CaeserCipher::encrypt(const char* text) const
 
 		if (current < BASE_LIMIT || current > CEIL_LIMIT) {
 			delete[] encryptedText;
-			throw std::invalid_argument("Invalid ASCII characters!");
+			throw std::invalid_argument("Invalid ASCII character!");
 		}
 
 		int normalised = current - BASE_LIMIT;
@@ -43,20 +44,51 @@ char* CaeserCipher::encrypt(const char* text) const
 
 char* CaeserCipher::decrypt(const char* text) const
 {
-	return nullptr;
+	if (!text || strlen(text) == 0) {
+		throw std::invalid_argument("Text can not be nullptr or empty!");
+	}
+
+	size_t textLength = strlen(text);
+	char* decryptedText = new char[textLength + 1];
+
+	for (size_t i = 0; i < textLength; i++) {
+		char current = text[i];
+
+		if (current < BASE_LIMIT || current > CEIL_LIMIT) {
+			delete[] decryptedText;
+			throw std::invalid_argument("Invalid ASCII character!");
+		}
+
+		int normalised = current - BASE_LIMIT;
+		normalised = utils::mod(normalised - this->shift, CEIL_LIMIT - BASE_LIMIT + 1);
+		decryptedText[i] = normalised + BASE_LIMIT;
+	}
+
+	decryptedText[textLength] = '\0';
+	return decryptedText;
 }
 
 void CaeserCipher::serialize(std::ostream& out) const
 {
+	if (!out) {
+		throw std::invalid_argument("Invalid output stream!");
+	}
+
+	out << getType() << '\n';
+	out << this->shift << '\n';
+
+	if (!out) {
+		throw std::runtime_error("Error while writing cipher! Possible corrupted writing!");
+	}
 }
 
 CaeserCipher* CaeserCipher::clone() const
 {
-	return nullptr;
+	return new CaeserCipher(*this);
 }
 
 const char* CaeserCipher::getType() const
 {
-	return nullptr;
+	return "CAESER";
 }
 
